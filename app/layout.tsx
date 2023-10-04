c399e6cc70f4972c4c7fdeb2b980f56efea8258a
Braimah-Abiola <braimahabiola.official@gmail.com>
2023-10-04 23:50:06 +0000

    Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ModalProvider />
-          {children}
+        {children}
         </body>
       </html>
     </ClerkProvider>
